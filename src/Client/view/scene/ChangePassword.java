8661fe38e4526dbc5b33743645daa0900e0503c2
zwoneee <trungtien.2808gl@gmail.com>
2025-12-26 23:53:26 +0700

    Commit: tất cả file

--- src/Client/view/scene/ChangePassword.java
+++ src/Client/view/scene/ChangePassword.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Client.view.scene;
 
 import Client.RunClient;
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author Hoang Tran < hoang at 99.hoangtran@gmail.com >
- */
 public class ChangePassword extends javax.swing.JFrame {
 
     /**
